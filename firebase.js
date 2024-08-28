1b077bdf256e5573f8c15e41de8c12ec662413c6
feliqe.developer <51057939+feliqe@users.noreply.github.com>
2024-08-28 09:34:32 -0400

    proyecto

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMd9bRefaC6qJv8fufvB5W40LDJYwWpzU",
-  authDomain: "chat-ac349.firebaseapp.com",
-  projectId: "chat-ac349",
-  storageBucket: "chat-ac349.appspot.com",
-  messagingSenderId: "461000930622",
-  appId: "1:461000930622:web:bd3fdd00c50fe5c4e9a86f"
+    apiKey: import.meta.env.VITE_APY_KEY,
+    authDomain: import.meta.env.VITE_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
